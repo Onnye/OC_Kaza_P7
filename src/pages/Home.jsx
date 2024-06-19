5773608e72ae8c3c001940508be0cca6a4d6c34c
Laura TUFO <laura.tufo94@gmail.com>
2024-06-19 14:09:03 +0200

    Add cards for Home + sass - Ok Desktop/Mobile

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Banner from "./../components/Banner";
+import CardContainer from "../components/Card/CardContainer";
+
+const Home = () => {
+  return (
+    <>
+      <Banner />
+      <div className="Home">
+        <CardContainer />
+      </div>
+    </>
+  );
+};
+
+export default Home;
